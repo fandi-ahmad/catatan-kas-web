6704db6c82e41933d5d7881553803a8d8c0122e5
fandi-ahmad <fandi4160@gmail.com>
2024-08-14 20:57:54 +0800

    modified button component add color style option

--- src/components/Button/BaseButton.tsx
+++ src/components/Button/BaseButton.tsx
@@ -1,16 +1,29 @@
 
 interface buttonProps {
   text: string
-  color: 'green' | 'red'
+  color: 'red' | 'green' | 'gray' | 'slate' | 'blue' | 'yellow'
   className?: string
   onClick?: () => void;
+  icon?: string
 }
 
 const BaseButton = (props: buttonProps) => {
+  let btnColor
+
+  switch (props.color) {
+    case 'red': btnColor = 'bg-red-500 hover:bg-red-400'; break;
+    case 'green': btnColor = 'bg-green-500 hover:bg-green-400'; break;
+    case 'gray': btnColor = 'bg-gray-500 hover:bg-gray-400'; break;
+    case 'slate': btnColor = 'bg-slate-500 hover:bg-slate-400'; break;
+    case 'blue': btnColor = 'bg-blue-500 hover:bg-blue-400'; break;
+    case 'yellow': btnColor = 'bg-yellow-500 hover:bg-yellow-400'; break;
+    default: break;
+  }
+
   return (
-    <button onClick={props.onClick} className={`${props.color == 'green' ? 'bg-green-500 hover:bg-green-400' : 'bg-red-500 hover:bg-red-400'} duration-200 text-white px-3 py-1 rounded-md ${props.className}`}>
-      <i className="fa-solid fa-plus"></i>
-      <span className="ms-1.5">{props.text}</span>
+    <button onClick={props.onClick} className={`${btnColor} duration-200 text-white px-3 py-1 rounded-md ${props.className}`}>
+      {props.icon ? <i className={`fa-solid me-1.5 ${props.icon}`}></i> : null}
+      <span>{props.text}</span>
     </button>
   )
 }
