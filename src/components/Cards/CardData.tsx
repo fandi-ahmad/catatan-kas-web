0441e3e47e8e8359f4b2b7a85aa737b4138e0ea6
fandi-ahmad <fandi4160@gmail.com>
2024-08-14 20:59:07 +0800

    make dynamic in data card

--- src/components/Cards/CardData.tsx
+++ src/components/Cards/CardData.tsx
@@ -1,7 +1,8 @@
+import { cashFormated } from "../../function"
 
 interface cardDataProps {
   note: string
-  amount: string
+  amount: number
   created_at: string
   type_cash: 'income' | 'spending'
 }
@@ -11,7 +12,7 @@ const CardData = (props: cardDataProps) => {
     <div className="w-full px-3 py-2 mb-3 rounded-md border border-1 border-slate-300 dark:border-slate-600 flex justify-between items-end">
       <div>
         <p className="text-sm">{props.note}</p>
-        <p className={`${props.type_cash === 'income' ? 'text-green-500' : 'text-red-500'} text-lg font-semibold`}>Rp. {props.amount}</p>
+        <p className={`${props.type_cash === 'income' ? 'text-green-500' : 'text-red-500'} text-lg font-semibold`}>Rp. {cashFormated(props.amount)}</p>
       </div>
       <div>
         <p className="text-sm opacity-75">{props.created_at}</p>
